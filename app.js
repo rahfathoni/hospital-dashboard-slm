0fa25d7313b9e58dd0480c20682f7ac55eddc05d
rahfathoni <rahfathoni@gmail.com>
2024-05-05 12:04:59 +0700

    init server

---
+++ app.js
@@ -0,0 +1,13 @@
+if(process.env.NODE_ENV == 'development'){
+  require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+module.exports = app;
